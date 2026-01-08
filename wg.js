d8ae6f161578042952f722ed8784535cbb022dfc
bpcllc110 <bpcllc110@gmail.com>
2026-01-08 23:28:17 +0800

    Update site URL in appConfig

--- wg.js
+++ wg.js
@@ -4,7 +4,7 @@ const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/6
 const appConfig = {
 	ver: 1,
 	title: '玩偶',
-	site: 'https://www.wogg.lol',
+	site: 'http://wogg.xxooo.cf',
 
 	tabs: [
 		{
